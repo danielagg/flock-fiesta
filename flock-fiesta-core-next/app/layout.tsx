7b5a89fa850b865f271d8bb3c8b7fad2e1c6d3db
Daniel Agg <daniel.agg@outlook.com>
2023-04-12 19:40:40 +0200

    restructure components

--- flock-fiesta-core-next/app/layout.tsx
+++ flock-fiesta-core-next/app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
-import { ClerkProvider, UserButton } from "@clerk/nextjs/app-beta";
-import Link from "next/link";
+import { ClerkProvider } from "@clerk/nextjs/app-beta";
 
 export const metadata = {
   title: "Flock Fiesta",
@@ -15,26 +14,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className="bg-gradient-to-tl from-purple-200 to-indigo-50 text-slate-900 min-h-screen">
-          <header className="block lg:flex justify-center p-6 bg-slate-900 lg:bg-transparent text-white lg:text-black">
-            <div className="w-full lg:w-2/3 flex flex-col lg:flex-row justify-center lg:justify-start items-center lg:space-x-24">
-              <div className="w-32 text-xl font-bold lowercase">
-                <Link href="/">Flock Fiesta</Link>
-              </div>
-              <nav className="w-full hidden lg:flex justify-between items-center">
-                <ul className="list-none flex flex-col lg:flex-row space-y-2 space-x-0 lg:space-y-0 lg:space-x-12 items-center mt-6 lg:mt-0">
-                  <li>How it works</li>
-                  <li>Pricing</li>
-                  <li>Contact</li>
-                </ul>
-                <UserButton />
-              </nav>
-            </div>
-          </header>
-          <div className="flex flex-col justify-center items-center mt-12 lg:mt-24">
-            {children}
-          </div>
-        </body>
+        <body>{children}</body>
       </html>
     </ClerkProvider>
   );
--- flock-fiesta-core-next/app/page.tsx
+++ flock-fiesta-core-next/app/page.tsx
@@ -1,25 +1,44 @@
-import { CallToAction } from "./call-to-action";
-import { Goal } from "./goal";
-import { HowDoesItWork } from "./how-does-it-work";
-import { Illustrations } from "./illustrations";
-import { Pricing } from "./pricing";
-import { Statistics } from "./statistics";
+import Link from "next/link";
+import { CallToAction } from "./components/call-to-action";
+import { Goal } from "./components/goal";
+import { HowDoesItWork } from "./components/how-does-it-work";
+import { Illustrations } from "./components/illustrations";
+import { Pricing } from "./components/pricing";
+import { Statistics } from "./components/statistics";
+import { UserButton } from "@clerk/nextjs/app-beta";
 
 export default function Home() {
   return (
-    <>
-      <div className="w-full lg:w-2/3 px-6">
-        <main className="flex flex-col lg:flex-row">
-          <CallToAction />
-          <Illustrations />
-        </main>
-        <Statistics />
-        <Goal />
-        <HowDoesItWork />
+    <body className="bg-gradient-to-tl from-purple-200 to-indigo-50 text-slate-900 min-h-screen">
+      <header className="block lg:flex justify-center p-6 bg-slate-900 lg:bg-transparent text-white lg:text-black">
+        <div className="w-full lg:w-2/3 flex flex-col lg:flex-row justify-center lg:justify-start items-center lg:space-x-24">
+          <div className="w-32 text-xl font-bold lowercase">
+            <Link href="/">Flock Fiesta</Link>
+          </div>
+          <nav className="w-full hidden lg:flex justify-between items-center">
+            <ul className="list-none flex flex-col lg:flex-row space-y-2 space-x-0 lg:space-y-0 lg:space-x-12 items-center mt-6 lg:mt-0">
+              <li>How it works</li>
+              <li>Pricing</li>
+              <li>Contact</li>
+            </ul>
+            <UserButton />
+          </nav>
+        </div>
+      </header>
+      <div className="flex flex-col justify-center items-center mt-12 lg:mt-24">
+        <div className="w-full lg:w-2/3 px-6">
+          <main className="flex flex-col lg:flex-row">
+            <CallToAction />
+            <Illustrations />
+          </main>
+          <Statistics />
+          <Goal />
+          <HowDoesItWork />
+        </div>
+        <div className="w-full bg-slate-900 flex justify-center">
+          <Pricing />
+        </div>
       </div>
-      <div className="w-full bg-slate-900 flex justify-center">
-        <Pricing />
-      </div>
-    </>
+    </body>
   );
 }
